f4e2591aec89aef4ef482ead44e3fe31a80bf67c
Nat Welch <nat@natwelch.com>
2019-07-05 16:35:49 +0000

    server and yarn

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const next = require('next');
+const sslRedirect = require('heroku-ssl-redirect');
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = parseInt(process.env.PORT, 10) || 3000
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+
+app.prepare().then(() => {
+  const server = express()
+    server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.use(sslRedirect());
+
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+});
