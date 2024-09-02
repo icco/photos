5627fbebb91e75327188518eda60f01d4cdd547b
Nat Welch <nat@natwelch.com>
2024-09-02 21:00:39 +0000

    init

---
+++ src/app/api/files/route.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { SignedPostPolicyV4Output } from "@google-cloud/storage";
+import { Storage } from "@google-cloud/storage";
+
+export default async function POST(
+  req: Request,
+) {
+  const storage = new Storage({
+    projectId: process.env.PROJECT_ID,
+    credentials: {
+      client_email: process.env.CLIENT_EMAIL,
+      private_key: process.env.PRIVATE_KEY,
+    },
+  });
+  const bucket = storage.bucket("icco-cloud");
+  const file = bucket.file(query.file as string);
+  const options = {
+    expires: Date.now() + 5 * 60 * 1000, //  5 minutes,
+    fields: { "x-goog-meta-source": "photos" },
+  };
+  const [response] = await file.generateSignedPostPolicyV4(options);
+  res.status(200).json(response);
+}
