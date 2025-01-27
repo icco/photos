cd4a761c1ab73d84dcfabc1c15a83acc2a4cf435
Nat Welch <nat@natwelch.com>
2025-01-27 01:43:44 +0000

    dsable

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,39 +11,39 @@ const config: Config = {
   ],
   plugins: [
     require("@tailwindcss/typography"),
-    require("daisyui"),
+    //require("daisyui"),
     require("@tailwindcss/forms")({
       strategy: "class", // only generate classes
     }),
   ],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
-          ...require("daisyui/src/theming/themes")["nord"],
-        },
-        dark: {
-          // eslint-disable-next-line @typescript-eslint/no-var-requires
-          "color-scheme": "dark",
-          primary: "#3B4252",
-          secondary: "#434C5E",
-          accent: "#81A1C1",
-          neutral: "#4C566A",
-          "base-content": "#eceff4",
-          "base-100": "#2E3440",
-          info: "#5E81AC",
-          success: "#A3BE8C",
-          warning: "#D08770",
-          error: "#BF616A",
-          "--rounded-box": "0.4rem",
-          "--rounded-btn": "0.2rem",
-          "--rounded-badge": "0.4rem",
-          "--tab-radius": "0.2rem",
-        },
-      },
-    ],
-    logs: false,
-  },
+  //  daisyui: {
+  //    themes: [
+  //      {
+  //        light: {
+  //          // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
+  //          ...require("daisyui/src/theming/themes")["nord"],
+  //        },
+  //        dark: {
+  //          // eslint-disable-next-line @typescript-eslint/no-var-requires
+  //          "color-scheme": "dark",
+  //          primary: "#3B4252",
+  //          secondary: "#434C5E",
+  //          accent: "#81A1C1",
+  //          neutral: "#4C566A",
+  //          "base-content": "#eceff4",
+  //          "base-100": "#2E3440",
+  //          info: "#5E81AC",
+  //          success: "#A3BE8C",
+  //          warning: "#D08770",
+  //          error: "#BF616A",
+  //          "--rounded-box": "0.4rem",
+  //          "--rounded-btn": "0.2rem",
+  //          "--rounded-badge": "0.4rem",
+  //          "--tab-radius": "0.2rem",
+  //        },
+  //      },
+  //    ],
+  //    logs: false,
+  //  },
 };
 export default config;
