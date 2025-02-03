d8ba1b2cd9bd225610823a23cbcfb36322546213
Nat Welch <nat@natwelch.com>
2025-02-03 03:18:16 +0000

    lint

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -10,9 +10,5 @@ type ThemeProviderProps = Parameters<typeof NextThemesProvider>[0];
  * 'use client' is essential for next-themes to work with app-dir.
  */
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props}>
-      {children}
-    </NextThemesProvider>
-  );
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
